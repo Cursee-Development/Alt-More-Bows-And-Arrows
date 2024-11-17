08cd9b94784b63530caedc464e03d53cf1977c53
jason13official <jason13official@gmail.com>
2024-11-16 20:58:01 -0500

    annotation lol

--- common/src/main/java/com/cursee/more_bows_and_arrows/core/entity/TieredArrowEntityRenderer.java
+++ common/src/main/java/com/cursee/more_bows_and_arrows/core/entity/TieredArrowEntityRenderer.java
@@ -4,6 +4,7 @@ import com.cursee.more_bows_and_arrows.Constants;
 import net.minecraft.client.renderer.entity.ArrowRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.NotNull;
 
 public class TieredArrowEntityRenderer extends ArrowRenderer<TieredArrowEntity> {
 
@@ -12,7 +13,7 @@ public class TieredArrowEntityRenderer extends ArrowRenderer<TieredArrowEntity> 
     }
 
     @Override
-    public ResourceLocation getTextureLocation(TieredArrowEntity tieredArrow) {
+    public @NotNull ResourceLocation getTextureLocation(TieredArrowEntity tieredArrow) {
         return new ResourceLocation(Constants.MOD_ID, "textures/entity/arrow/" + tieredArrow.getVariant().getSerializedName() + "_arrow.png");
     }
 }
