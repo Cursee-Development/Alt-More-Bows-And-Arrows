1d6f316e22a4388a84bbed823b29fcb0de240eb8
jason13official <jason13official@gmail.com>
2024-11-19 20:45:08 -0500

    fix mixin, begin interactions

--- common/src/main/java/com/cursee/more_bows_and_arrows/core/item/TieredBowItem.java
+++ common/src/main/java/com/cursee/more_bows_and_arrows/core/item/TieredBowItem.java
@@ -2,10 +2,17 @@ package com.cursee.more_bows_and_arrows.core.item;
 
 import com.cursee.more_bows_and_arrows.core.tier.BowTier;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -40,5 +47,70 @@ public class TieredBowItem extends BowItem {
         components.add(Component.translatable("lore.more_bows_and_arrows." + this.tier.toString().toLowerCase() + "_bow_damage"));
 
         super.appendHoverText(itemStack, level, components, flag);
+    }
+
+    public void playerAttacksEntity(Player player, LivingEntity livingEntity) {
+        switch (this.tier) {
+            case STRIPPED_WOOD -> {
+            }
+            case WOOD -> {
+            }
+            case STRIPPED_NETHER_WOOD -> {
+            }
+            case NETHER_WOOD -> {
+            }
+            case STRIPPED_BAMBOO -> {
+            }
+            case BAMBOO -> {
+            }
+            case AMETHYST -> {
+            }
+            case BLAZE_ROD -> {
+            }
+            case BONE -> {
+            }
+            case COPPER -> {
+            }
+            case DIAMOND -> {
+            }
+            case EMERALD -> {
+            }
+            case END_STONE -> {
+            }
+            case GOLD -> {
+            }
+            case IRON -> {
+            }
+            case LAPIS -> {
+            }
+            case LIGHTNING_ROD -> {
+                this.lightningRodBowInteraction(player, livingEntity);
+            }
+            case NETHERITE -> {
+            }
+            case OBSIDIAN -> {
+            }
+            case PAPER -> {
+            }
+            case PRISMARINE -> {
+            }
+        }
+    }
+
+    private void lightningRodBowInteraction(Player player, LivingEntity livingEntity) {
+
+        Level level = livingEntity.level();
+
+        LightningBolt bolt = EntityType.LIGHTNING_BOLT.create(level);
+        if (bolt == null) return;
+
+        bolt.moveTo(Vec3.atBottomCenterOf(livingEntity.blockPosition()));
+        if (player instanceof ServerPlayer serverPlayer) bolt.setCause(serverPlayer);
+
+        level.addFreshEntity(bolt);
+    }
+
+    public void playerAttacksBlock(Player player, BlockHitResult result) {
+
     }
 }
--- common/src/main/java/com/cursee/more_bows_and_arrows/mixin/CommonAbstractArrowMixin.java
+++ common/src/main/java/com/cursee/more_bows_and_arrows/mixin/CommonAbstractArrowMixin.java
@@ -5,6 +5,7 @@ import com.cursee.more_bows_and_arrows.core.item.TieredBowItem;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.EntityHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractArrow.class)
-public class FabricAbstractArrowMixin {
+public class CommonAbstractArrowMixin {
 
     /** Occurs when an entity is actually being hurt, after a potion effect would have been applied by an arrow. */
     @Inject(method = "onHitEntity", at= @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/world/item/enchantment/EnchantmentHelper;doPostHurtEffects(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/Entity;)V"))
@@ -23,10 +24,20 @@ public class FabricAbstractArrowMixin {
 
         if (instance.getOwner() instanceof Player player && player.getMainHandItem().getItem() instanceof TieredBowItem tieredBowItem) {
             livingEntity.hurt(livingEntity.damageSources().playerAttack(player), tieredBowItem.tier.bonusDamage());
+            tieredBowItem.playerAttacksEntity(player, livingEntity);
         }
 
         if (instance instanceof TieredArrowEntity tieredArrowEntity) {
             livingEntity.hurt(livingEntity.damageSources().arrow(instance, tieredArrowEntity.getOwner()), tieredArrowEntity.bonusDamage());
+        }
+    }
+
+    @Inject(method = "onHitBlock", at = @At("TAIL"))
+    private void injected$onHitBlock(BlockHitResult result, CallbackInfo ci) {
+
+        AbstractArrow instance = (AbstractArrow) (Object) this;
+        if (instance.getOwner() instanceof Player player && player.getMainHandItem().getItem() instanceof TieredBowItem tieredBowItem) {
+            tieredBowItem.playerAttacksBlock(player, result);
         }
     }
 }
