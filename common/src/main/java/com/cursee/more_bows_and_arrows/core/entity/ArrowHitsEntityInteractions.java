28512b59f759b15c390867578e6440a7e64ef972
jason13official <jason13official@gmail.com>
2024-11-19 20:45:31 -0500

    simplify arrow interaction

--- common/src/main/java/com/cursee/more_bows_and_arrows/core/entity/ArrowHitsEntityInteractions.java
+++ common/src/main/java/com/cursee/more_bows_and_arrows/core/entity/ArrowHitsEntityInteractions.java
@@ -1,6 +1,10 @@
 package com.cursee.more_bows_and_arrows.core.entity;
 
+import com.cursee.more_bows_and_arrows.core.item.TieredBowItem;
+import com.cursee.more_bows_and_arrows.core.tier.BowTier;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.EntityHitResult;
 import org.jetbrains.annotations.Nullable;
 
@@ -8,13 +12,10 @@ public class ArrowHitsEntityInteractions {
 
     public static void interact(TieredArrowEntity tieredArrowEntity, @Nullable Entity nullableOwner, EntityHitResult entityHitResult) {
 
-
+        if (!(nullableOwner instanceof Player player)) return;
 
         switch (tieredArrowEntity.getVariant()) {
-            case AMETHYST -> {
-            }
-            case BAMBOO -> {
-            }
+            case AMETHYST, BAMBOO -> {/* NO-OP */}
             case BLAZE_ROD -> {
             }
             case BONE -> {
@@ -62,6 +63,7 @@ public class ArrowHitsEntityInteractions {
             case LAPIS -> {
             }
             case LIGHTNING_ROD -> {
+                if (player.getMainHandItem().getItem() instanceof TieredBowItem tieredBowItem && tieredBowItem.tier == BowTier.LIGHTNING_ROD) tieredArrowEntity.level().explode(player, entityHitResult.getEntity().xo, entityHitResult.getEntity().yo, entityHitResult.getEntity().zo, 4.0f, Level.ExplosionInteraction.TNT);
             }
             case NETHERITE -> {
             }
