9ca9c152c04531afb3688b635e974088d901ac20
jason13official <jason13official@gmail.com>
2024-11-25 17:49:27 -0500

    spawn egg at relative location

--- common/src/main/java/com/cursee/more_bows_and_arrows/core/entity/ArrowHitsBlockInteractions.java
+++ common/src/main/java/com/cursee/more_bows_and_arrows/core/entity/ArrowHitsBlockInteractions.java
@@ -77,7 +77,8 @@ public class ArrowHitsBlockInteractions {
             case EGG -> {
                 Chicken chicken = EntityType.CHICKEN.create(player.level());
                 if (chicken != null) {
-                    chicken.moveTo(blockHitResult.getBlockPos().getX(), blockHitResult.getBlockPos().getY(), blockHitResult.getBlockPos().getZ());
+
+                    chicken.moveTo(relative.getX(), relative.getY(), relative.getZ());
                     level.addFreshEntity(chicken);
                 }
             }
