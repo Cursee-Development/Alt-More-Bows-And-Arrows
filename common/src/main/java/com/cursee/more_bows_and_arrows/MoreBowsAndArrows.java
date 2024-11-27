d8ba38653e7e95f46c0ea2a5e25c666a58212d40
jason13official <jason13official@gmail.com>
2024-11-27 15:40:43 -0500

    dev log

--- common/src/main/java/com/cursee/more_bows_and_arrows/MoreBowsAndArrows.java
+++ common/src/main/java/com/cursee/more_bows_and_arrows/MoreBowsAndArrows.java
@@ -1,6 +1,10 @@
 package com.cursee.more_bows_and_arrows;
 
+import com.cursee.more_bows_and_arrows.platform.Services;
+
 public class MoreBowsAndArrows {
 
-    public static void init() {}
+    public static void init() {
+        if (Services.PLATFORM.isDevelopmentEnvironment()) Constants.LOG.info("Initialized core in development environment!");
+    }
 }
